3f4bdaa0faf4306942ccf03a731d25e6152b9079
chucoyos <unodev@outlook.com>
2025-04-22 16:38:39 -0600

    Move Header into a component

--- app/page.js
+++ app/page.js
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import Video from "./components/video";
+import Header from "./components/header";
 
 export default function Home() {
   return (
@@ -18,54 +19,7 @@ export default function Home() {
       <div className="absolute inset-0 bg-black/50"></div>
   
       <main className="relative z-10 flex flex-col gap-[32px] row-start-2 items-center sm:items-start text-white">
-        <h1 className="text-4xl sm:text-5xl font-bold tracking-[-.01em] text-center sm:text-left">
-          Yaco YardContainers
-        </h1>
-        <h2 className="text-2xl sm:text-3xl font-semibold tracking-[-.01em] text-center sm:text-left">
-          Gestiona tu patio de contenedores.
-        </h2>
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)] text-white/90">
-          <li className="tracking-[-.01em]">
-            Asigna y controla la ubicación de cada contenedor.
-          </li>
-          <li className="tracking-[-.01em]">
-            Genera EIR con Código QR.
-          </li>
-          <li className="tracking-[-.01em]">
-            Genera servicios de manera automática.
-          </li>
-          <li className="tracking-[-.01em]">
-            Genera estados de cuenta de manera automática.
-          </li>
-          <li className="tracking-[-.01em]">
-            Asigna línea de crédito a tus clientes.
-          </li>
-          <li className="tracking-[-.01em]">
-            Notificaciones en tiempo real.
-          </li>
-          <li className="tracking-[-.01em]">
-            Exporta reportes a Excel y mucho más.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-white text-black gap-2 hover:bg-gray-200 font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://wa.me/523141352209?text=Estoy%20interesado%20en%20la%20aplicación%20Yaco%20YardContainers"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Contratar
-          </a>
-          <a
-            className="rounded-full border border-solid border-white transition-colors flex items-center justify-center hover:bg-white/10 font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px] text-white"
-            href="https://wa.me/523141352209?text=Estoy%20interesado%20en%20la%20aplicación%20Yaco%20YardContainers"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Información
-          </a>
-        </div>
+        <Header />
 
         {/* New Video Grid Section */}
         <section className="relative z-10 w-full max-w-6xl row-start-3 px-4 sm:px-0" id="videos">
